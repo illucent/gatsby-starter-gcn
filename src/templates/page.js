bf51f506275917ed180feed17e68f05a3611feae
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-15 17:42:44 +0200

    Update page.js

--- src/templates/page.js
+++ src/templates/page.js
@@ -31,6 +31,11 @@ export const query = graphql`
     contentfulPage(slug: { eq: $slug }) {
       title
       slug
+      metaDescription {
+        internal {
+          content
+        }
+      }
       body {
         childMarkdownRemark {
           html
